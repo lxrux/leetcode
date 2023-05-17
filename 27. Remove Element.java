77e086143c1c5d6cc1ff7bdab3fe317bee0716f7
Ruslan Tsvetkov <ruslan.tsvetkov@gmail.com>
2023-05-17 15:56:16 +0300

    Update 27. Remove Element.java
    100%/90.84%

--- 27. Remove Element.java
+++ 27. Remove Element.java
@@ -1,14 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int k = 0;
-        int i = 0;
-        while (i < nums.length) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] == val)  {
-                i++;
                 continue;
             }
 
-            nums[k++] = nums[i++];
+            nums[k++] = nums[i];
         }
         return k;
     }
