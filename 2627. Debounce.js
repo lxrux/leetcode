a3b05c2221f89752830c9b8f4314f2cc18991698
Ruslan Tsvetkov <ruslan.tsvetkov@gmail.com>
2023-08-28 15:46:55 +0300

    2627. Debounce.js

---
+++ 2627. Debounce.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timeoutId;
+
+    return function(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn(...args), t);
+    }
+};
